90ecaaf735ed4bc29183a99acc42460d805cf3e7
markovAntony <abbrown22@gmail.com>
2016-02-19 15:58:08 -0700

    Add spec files

---
+++ spec/lib/cadooz/models.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Cadooz::Model do
+  let(:address) { Object.new }
+  let(:attributes) { Object.new }
+  let(:catalog) { Object.new }
+  let(:catalog_product) { Object.new }
+  let(:catalog_product_variation) { Object.new }
+  let(:generation_profile_product) { Object.new }
+  let(:greeting_card) { Object.new }
+  let(:invoice_information) { Object.new }
+  let(:order) { Object.new }
+  let(:order_position) { Object.new }
+  let(:order_status) { Object.new }
+  let(:payment) { Object.new }
+  let(:product_category) { Object.new }
+  let(:voucher) { Object.new }
+end
---
+++ spec/lib/cadooz/services.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Cadooz::BusinessOrderService do
+
+end
