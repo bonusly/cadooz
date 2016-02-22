2a4c0d08bfb21990ac680a1bbf321a54e0cda5e8
markovAntony <abbrown22@gmail.com>
2016-02-22 16:39:12 -0700

    Started adding services specs

--- spec/lib/cadooz/services.rb
+++ spec/lib/cadooz/services.rb
@@ -3,44 +3,68 @@ require 'spec_helper'
 describe Cadooz::BusinessOrderService do
   include CadoozHelpers
 
+  let(:order) { create_order_param }
+
   describe "instance methods" do
-    context "create order" do
+    describe "create order" do
+      context "succeeds" do
+        let(:response) { sample_create_order_response(true) }
+
+        before do
+          double(Cadooz::BusinessOrderService).create_order(order) { response }
+        end
+
+        it "should create the order" do
+          double(Cadooz::Immutable::Order).new(response) { true }
+        end
+      end
+
+      context "fails" do
+        let(:response) { sample_create_order_response(false) }
+
+        before do
+          double(Cadooz::BusinessOrderService).create_order(order) { response }
+        end
+
+        # it "should throw an exception"
+        #   # TODO
+        # end
+      end
+    end
+
+    describe "get order status" do
 
     end
 
-    context "get order status" do
+    describe "get order status by customer reference number" do
 
     end
 
-    context "get order status by customer reference number" do
+    describe "get changed orders" do
 
     end
 
-    context "get changed orders" do
+    describe "get order" do
 
     end
 
-    context "get order" do
+    describe "get available catalogs" do
 
     end
 
-    context "get available catalogs" do
+    describe "get product informations" do
 
     end
 
-    context "get product informations" do
+    describe "get available categories" do
 
     end
 
-    context "get available categories" do
+    describe "get available products" do
 
     end
 
-    context "get available products" do
-
-    end
-
-    context "get vouchers for order" do
+    describe "get vouchers for order" do
 
     end
   end
--- spec/support/helpers/cadooz_helpers.rb
+++ spec/support/helpers/cadooz_helpers.rb
@@ -1,3 +1,35 @@
 module CadoozHelpers
-  
+  def create_order_param
+    order = Cadooz::Mutable::Order.new
+    address = Cadooz::Mutable::Address.new
+    position = Cadooz::Mutable::OrderPosition.new
+
+    address.city = 'Springfield'
+    address.company = 'Nuclear Power Plant'
+    address.country = 'US'
+    address.department = 'Safety'
+    address.email = 'homer.simpson@springfieldnuclear.biz'
+    address.firstname = 'Homer'
+    address.lastname = 'Simpson'
+    address.phone = '(555) 555-5555'
+    address.salutation = 'Mr.'
+    address.state = 'OR'
+    address.street = '100 Industrial Way'
+    address.zipcode = 97477
+
+    position.amount = 1
+    position.cadooz_product_number = 8099
+    position.currency = 'EUR'
+    position.value = 10
+
+    order.customer_reference_number = 1
+    order.delivery_address = address
+    order.order_positions << position
+
+    order
+  end
+
+  def sample_create_order_response(success)
+
+  end
 end
