a6a14f32940376ac8e8ed234192fa557b8af0576
markovAntony <abbrown22@gmail.com>
2016-02-22 15:09:08 -0700

    Improve models spec

--- spec/lib/cadooz/models.rb
+++ spec/lib/cadooz/models.rb
@@ -2,45 +2,56 @@ require 'spec_helper'
 
 describe Cadooz do
 
+  RSpec::Matchers.define :mutable_block do
+    match do |actual|
+      actual.is_a? Proc
+      actual.call == 'test'
+    end
+
+    def supports_block_expectations?
+      true
+    end
+  end
+
   describe "immutable class attributes" do
-    Dir.foreach(Dir.pwd + '/lib/cadooz/models/immutable') do |model|
+    Dir.foreach('./lib/cadooz/models/immutable') do |model|
       next if ['.', '..'].include?(model)
 
       name = 'Cadooz::Immutable::' + model.chomp('.rb').split('_').collect(&:capitalize).join
       immutable_class = Object::const_get(name).new(OpenStruct.new)
 
-      it "should throw an exception when attempting to set an instance variable" do
+      context "set instance variables" do
         immutable_class.instance_variables.each do |var|
-          expect { immutable_class.instance_variable_set(var, 'test') }.to raise_error(RuntimeError)
+          specify { expect { immutable_class.instance_variable_set(var, 'test') }.to raise_error(RuntimeError) }
         end
       end
 
-      it "should throw an exception when attempting to assign to an instance variable" do
+      context "assign instance variables" do
         immutable_class.instance_variables.each do |var|
           plain_name = var.to_s.gsub(':@', '')
-          expect { immutable_class.send(plain_name + '=', 'test') }.to raise_error(NoMethodError)
+          specify { expect { immutable_class.send(plain_name + '=', 'test') }.to raise_error(NoMethodError) }
         end
       end
     end
   end
 
   describe "mutable class attributes" do
-    Dir.foreach(Dir.pwd + '/lib/cadooz/models/mutable') do |model|
+    Dir.foreach('./lib/cadooz/models/mutable') do |model|
       next if ['.', '..'].include?(model)
 
       name = 'Cadooz::Mutable::' + model.chomp('.rb').split('_').collect(&:capitalize).join
       mutable_class = Object::const_get(name).new
 
-      it "should succeed when attemption to set an instance variable" do
+      context "set instance variables" do
         mutable_class.instance_variables.each do |var|
-          expect { mutable_class.instance_variable_set(var, 'test') }.to be_truthy
+          specify { expect { mutable_class.instance_variable_set(var, 'test') }.to mutable_block }
         end
       end
 
-      it "should succeed when attempting to assign to an instance variable" do
+      context "assign instance variables" do
         mutable_class.instance_variables.each do |var|
-          plain_name = var.to_s.gsub(':@', '')
-          expect { mutable_class.send(plain_name + '=', 'test') }.to be_truthy
+          plain_name = var.to_s.gsub('@', '')
+          specify { expect { mutable_class.send(plain_name + '=', 'test') }.to mutable_block }
         end
       end
     end
