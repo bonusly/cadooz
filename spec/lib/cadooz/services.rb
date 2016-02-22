a6ed1f07e106a83387c076c3d25e84bad41c251c
markovAntony <abbrown22@gmail.com>
2016-02-22 14:16:27 -0700

    Add contexts for service specs

--- spec/lib/cadooz/services.rb
+++ spec/lib/cadooz/services.rb
@@ -2,4 +2,45 @@ require 'spec_helper'
 
 describe Cadooz::BusinessOrderService do
 
+  describe "instance methods" do
+    context "create order" do
+
+    end
+
+    context "get order status" do
+
+    end
+
+    context "get order status by customer reference number" do
+
+    end
+
+    context "get changed orders" do
+
+    end
+
+    context "get order" do
+
+    end
+
+    context "get available catalogs" do
+
+    end
+
+    context "get product informations" do
+
+    end
+
+    context "get available categories" do
+
+    end
+
+    context "get available products" do
+
+    end
+
+    context "get vouchers for order" do
+
+    end
+  end
 end
