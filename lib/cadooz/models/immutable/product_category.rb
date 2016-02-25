c1f246b4dce9f59de7d3d1cdded4e784409b1769
markovAntony <abbrown22@gmail.com>
2016-02-25 11:03:12 -0700

    Correct nested serialization for ::Immutable::ProductCategory

--- lib/cadooz/models/immutable/product_category.rb
+++ lib/cadooz/models/immutable/product_category.rb
@@ -6,6 +6,9 @@ class Cadooz::Immutable::ProductCategory
   def initialize(open_struct)
     @id = open_struct&.id
     @description = open_struct&.description
+                              &.map_entries
+                              &.elements
+                              &.inject({}) { |hash, element| hash.merge(element.key.to_sym => element.value) }
     @internal_name = open_struct&.internal_name
     @shop_name = open_struct&.shop_name
                             &.map_entries
